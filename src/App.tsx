e2d0056ff2a1722ec248a38ddcdcb154620799be
tracy zumbu garcia <tracyzumbu@lilo.org>
2020-09-20 14:26:37 +0200

    added questionsCard

--- src/App.tsx
+++ src/App.tsx
@@ -18,17 +18,31 @@ type AnswerObject = {
 
 const App = () => {
 
-  const [loadinng, setLoadinng] = useState(false)
+  const [loading, setLoading] = useState(false)
   const [questions, setQuestions] = useState<QuestionState[]>([])// this will be a array from QuestionState
   const [number, setNumber] = useState(0)
   const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([])
   const [score, setScore] = useState(0)
   const [gameOver, setGameOver] = useState(true)
 
-  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY ));
+  console.log(questions);
   
 
   const startTrivia = async () => {
+    setLoading(true)
+    setGameOver(false)
+
+    const newQuestions = await fetchQuizQuestions(
+      TOTAL_QUESTIONS,
+      Difficulty.EASY
+    );
+
+    setQuestions(newQuestions);
+    setScore(0)
+    setUserAnswers([])
+    setNumber(0)
+    setLoading(false);
+
 
   }
 
@@ -43,17 +57,20 @@ const App = () => {
   return (
     <div className="App">
       <h1>React quiz</h1>
-      <button className="start" onClick={startTrivia}>Start</button>
-      <p className="score">Score:</p>
-      <p>Loading Questions ...</p>
-      {/* <QuestionsCard
+      { gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+        <button className="start" onClick={startTrivia}>Start</button>
+      ) : null }
+      { !gameOver ? <p className="score">Score:</p> : null }
+      { loading && <p>Loading Questions ...</p> }
+      { !loading && !gameOver && ( <QuestionsCard
         questionNr={number + 1}
         totalQuestions={TOTAL_QUESTIONS}
         question={questions[number].question}
         answers={questions[number].answers}
         userAnswer={userAnswers ? userAnswers[number] : undefined}
         callback={checkAnswer}
-      /> */}
+      />
+      )}
       <button className="next" onClick={nextQuestion}>Next question</button>
 
     </div>
